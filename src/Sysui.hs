985277b2a3f7e2385391aada63f9e1b17dd68728
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-06-12 18:56:37 +0100

    refactor a large part of the code and implement configuration

--- src/Sysui.hs
+++ src/Sysui.hs
@@ -7,31 +7,47 @@ import Control.Monad
 import Control.Monad.Trans
 import Data.Hourglass
 import System.Hourglass
+import System.Process (readProcessWithExitCode)
 import Graphics.UI.Gtk
 
 import Battery
 
-debug = False
-
-withSection box name f = do
-    lbl <- labelNew (Just name)
-    boxPackStart box lbl PackNatural 2
-
-    (widget, a) <- f
-    boxPackStart box widget PackNatural 2
-    return a
+-- Configuration interface:
 
 timeUnit = 500 -- in milliseconds
 seconds v = v * (1000 `div` timeUnit)
-batteryUpdateTime = seconds 30
 
-getCurrentFormattedTime tz = do
-    let pdt = TimezoneOffset (-7*60)
-    e <- localTimeFromGlobal <$> timeCurrent
-    let localMy = localTimeSetTimezone tz e
-        localPdt = localTimeSetTimezone pdt e
-        fmt = "YYYY-MM-DD H:MI:S" :: String
-    return (localTimePrint fmt localMy, localTimePrint fmt localPdt)
+defaultTimeFormat :: String
+defaultTimeFormat = "YYYY-MM-DD H:MI:S"
+
+defaultTimeToShow :: IO TimeToShow
+defaultTimeToShow = timezoneCurrent >>= \tz -> return $ TimeToShow "Local" tz
+
+defaultBatteryRefreshingTimer :: Int
+defaultBatteryRefreshingTimer = seconds 30
+
+data TimeToShow = TimeToShow
+    { getTSLabel_  :: String
+    , getTSOffset_ :: TimezoneOffset
+    }
+
+data SysuiConfig = SysuiConfig
+    { timeFormat :: String
+    , timeToShow :: [TimeToShow]
+    , showCalendar :: Bool
+    , batteryRefreshingTime :: Int
+    }
+
+defaultSysuiConfig :: IO SysuiConfig
+defaultSysuiConfig =
+    defaultTimeToShow >>= \currentTZ ->
+        return $ SysuiConfig
+                    defaultTimeFormat -- set the default time format
+                    [ currentTZ ]     -- set the default list of clock to show
+                    True              -- need the calendar
+                    defaultBatteryRefreshingTimer
+
+-- Battery
 
 createBatteryWidget box bat = do
     h   <- hBoxNew False 20
@@ -72,64 +88,53 @@ updateBatteryWidget wl bat = do
             let showCapacity_ c = (show c) ++ "%"
             in  maybe "N/A" showCapacity_ $ getCapacity_ bi
 
+-- AC
+
 createACWidget box ac = do
     h   <- hBoxNew False 20
     lbl <- labelNew (Just "")
-    lb  <- levelBarNew
 
-    set lb [ levelBarMinValue := 0.0, levelBarMaxValue := 100.0 ]
 
     boxPackStart h lbl PackNatural 2 
-    boxPackStart h lb PackNatural 2
     boxPackStart box h PackNatural 2
-    return (ac, (lbl, lb))
+    return (ac, lbl)
 
 updateACWidget wl ac = do
     st <- isACOnline ac
     let status = if st then "ONLINE" else "OFFLINE"
     case lookup ac wl of
-        Nothing             -> return ()
-        Just (lblWidget, _) -> labelSetText lblWidget (ac ++ ": " ++ status)
+        Nothing        -> return ()
+        Just lblWidget -> labelSetText lblWidget (ac ++ ": " ++ status)
 
-main = do
-    ctrRef <- newIORef 0
-    _ <- initGUI
+-- Clock
 
-    tz <- timezoneCurrent
-    Just disp <- displayGetDefault
-    nScreens  <- displayGetNScreens disp
-    --screen    <- displayGetScreen disp 0
-    --nMonitors <- screenGetNMonitors 0
-    --monitorSize <- screenGetMonitorGeometry screen onMonitor
+createClockWidget box tts = do
+    lbl <- labelNew (Nothing)
+    boxPackStart box lbl PackNatural 2 
+    return (getTSLabel_ tts, lbl) 
 
-    when debug $ do
-        putStrLn ("screens : " ++ show nScreens)
-        --putStrLn ("monitors: " ++ show nMonitors)
-        --putStrLn ("monitor: "  ++ show monitorSize)
+updateClockWidget e fmt wl (TimeToShow label offset) =
+    let t    = localTimeSetTimezone offset e
+        fmtT = localTimePrint fmt t
+    in  case lookup label wl of
+            Nothing -> return ()
+            Just w -> labelSetText w (label ++ ": " ++ fmtT)
 
-    --let Rectangle x y w _ = monitorSize
+-- Graphics
 
-    window <- windowNew
-    widgetSetName window "sysui"
+-- withSection :: (BoxClass b, WidgetClass w) => b -> String -> IO (w, y) -> y
+withSection box name f = do
+    lbl <- labelNew (Just name)
+    boxPackStart box lbl PackNatural 2
 
-    box <- vBoxNew False 15
-    containerAdd window box
+    (widget, a) <- f
+    boxPackStart box widget PackNatural 2
+    return a
 
-    clockUpdate <- withSection box "Clock" $ do
-        clockBox <- vBoxNew False 15
-        (iniL, iniPdt) <- getCurrentFormattedTime tz
-        lblLocal <- labelNew (Just ("local time: " ++ iniL))
-        lblPdt   <- labelNew (Just ("PDT time: " ++ iniPdt))
-        boxPackStart clockBox lblLocal PackNatural 2
-        boxPackStart clockBox lblPdt PackNatural 2
-        let setter = getCurrentFormattedTime tz >>= \(l, pst) -> do
-                        labelSetText lblLocal ("local time: " ++ l)
-                        labelSetText lblPdt ("PDT time: " ++ pst)
-        return (clockBox, setter)
-
-
-    powerUpdate <- withSection box "Power Supply" $ do
-        powerBox <- vBoxNew False 15
+createPowerSupplyWidget :: BoxClass b => b -> SysuiConfig -> IO (IO ())
+createPowerSupplyWidget box _ =
+    withSection box "Power Supply" $ do
+        powerBox <- vBoxNew True 15
 
         (PowerSupplies batList acList _) <- enumerateBatteries
 
@@ -149,16 +154,75 @@ main = do
         updatePower
         return (powerBox, updatePower)
 
-    withSection box "Calendar" $ do
-        c <- calendarNew
-        return (c, ())
+createTimeWidget :: BoxClass b => b -> SysuiConfig -> IO (IO ())
+createTimeWidget box config
+    | not $ null $ timeToShow config =
+        withSection box "Clock" $ do
+            clocksBox <- vBoxNew False 15
+            clockWidgets <- mapM (createClockWidget clocksBox) (timeToShow config)
+            let updateClocks = (localTimeFromGlobal <$> timeCurrent) >>= \refLocal ->
+                                    forM_ (timeToShow config) (updateClockWidget refLocal (timeFormat config) clockWidgets)
+            updateClocks
+            return (clocksBox, updateClocks)
+    | otherwise = return $ return ()
 
+createCalendarWidget :: BoxClass b => b -> SysuiConfig -> IO ()
+createCalendarWidget box config
+    | (showCalendar config) == True =
+        withSection box "Calendar" $ do
+            c <- calendarNew
+            return (c, ())
+    | otherwise = return ()
+
+-- myOwnConfiguration
+
+mySysuiConfig :: IO SysuiConfig
+mySysuiConfig = do
+    currentTTS <- defaultTimeToShow
+    (_, t, _) <- readProcessWithExitCode "date" ["+%Z"] ""
+    return $ SysuiConfig
+                defaultTimeFormat
+                (listOfTimeToShow currentTTS t)
+                False
+                defaultBatteryRefreshingTimer
+    where
+        listOfTimeToShow currentTTS t =
+            [ currentTTS
+            , TimeToShow "UTC" timezone_UTC
+            , TimeToShow "France" $ TimezoneOffset $ parisTZOffset t
+            ]
+        parisTZOffset t =
+            case t of
+                "BST\n" -> 2 * 60
+                "GMT\n" -> 1 * 60
+                _       -> 1 * 60
+
+-- Default main:
+defaultMain :: SysuiConfig -> IO ()
+defaultMain sysuiConfig = do
+    ctrRef <- newIORef 0
+    _ <- initGUI
+
+    -- Create the main application's window
+    window <- windowNew
+    widgetSetName window "sysui"
+
+    -- Create the main applications's box
+    box <- vBoxNew False 15
+    containerAdd window box
+
+    -- generate the needed widgets
+    powerUpdate <- createPowerSupplyWidget box sysuiConfig
+    timeUpdate <- createTimeWidget box sysuiConfig
+    _ <- createCalendarWidget box sysuiConfig
+
+    -- Timer to update the different widget which need to be updated periodicaly
     flip timeoutAdd timeUnit $ do
         ctr <- readIORef ctrRef
-        when ((ctr `mod` batteryUpdateTime) == 0) powerUpdate
+        when ((ctr `mod` (batteryRefreshingTime sysuiConfig)) == 0) powerUpdate
 
         -- update date every now and then
-        clockUpdate
+        timeUpdate
 
         writeIORef ctrRef (ctr+1)
         return True
@@ -174,3 +238,7 @@ main = do
     widgetShowAll window
 
     mainGUI
+
+-- Run the default main with the default configuration file
+main = mySysuiConfig >>= defaultMain
+
